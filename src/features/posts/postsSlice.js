a18ec576ccadd7577b1c94a67ce6436349a1307e
alicalimli <alicalimli76@gmail.com>
2022-07-23 14:35:08 +0300

    added prettier

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,22 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [{
-  id:1, title: "blahblahblah", content: "asfsdf",
-  id:2, title: "blahblahblah", content: "asfsdf",
-}]
+const initialState = [
+  {
+    id: 1,
+    title: "blahblahblah",
+    content: "asfsdf",
+    id: 2,
+    title: "blahblahblah",
+    content: "asfsdf",
+  },
+];
 
 const postsSlice = createSlice({
-  name: 'posts',
+  name: "posts",
   initialState,
   reducers: {
-    addPost: (state,action) => {
-      state.push(action.payload)
+    addPost: (state, action) => {
+      state.push(action.payload);
     },
-  }
-})
+  },
+});
 
 export const selectPosts = (state) => state.posts;
 
-export const {addPost} = postsSlice.actions;
+export const { addPost } = postsSlice.actions;
 
 export default postsSlice.reducer;
