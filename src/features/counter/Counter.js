309c9f96187e0886e8a289446a044c683823f84e
alicalimli <alicalimli76@gmail.com>
2022-07-23 12:06:33 +0300

    added incrementByAmount action

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,19 +1,31 @@
 
 import {useDispatch, useSelector} from 'react-redux'
-import {increment,decrement,reset} from './counterSlice'
+import {increment,decrement,reset,incrementByAmount} from './counterSlice'
+
+import {useState} from 'react'
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
 
-  console.log(decrement,increment);
+  const [amount,setAmount] = useState(0)
+  const addAmountValue = Number(amount) || 0;
+
+  const resetAll = () => {
+    setAmount(0)
+    dispatch(reset())
+  }
 
   return (
     <>
       <div>{count}</div>
       <button onClick={()=> dispatch(increment())}>+</button>  
       <button onClick={()=> dispatch(decrement())}>-</button>  
-      <button onClick={()=> dispatch(reset())}>reset</button>  
+      <button onClick={()=> dispatch(resetAll())}>reset</button>  
+      <div>
+        <input type="text" value={amount} onChange={(e)=> setAmount(e.target.value)}/>
+        <button onClick={()=> dispatch(incrementByAmount(addAmountValue))}>add value</button>
+      </div>
     </>
   )
 }
