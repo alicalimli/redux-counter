7f18c9bfcd6309bd67bb563919a6733a6bbf5bcf
alicalimli <alicalimli76@gmail.com>
2022-07-23 12:00:14 +0300

    added reset action

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,6 +1,6 @@
 
 import {useDispatch, useSelector} from 'react-redux'
-import {increment,decrement} from './counterSlice'
+import {increment,decrement,reset} from './counterSlice'
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
@@ -13,6 +13,7 @@ const Counter = () => {
       <div>{count}</div>
       <button onClick={()=> dispatch(increment())}>+</button>  
       <button onClick={()=> dispatch(decrement())}>-</button>  
+      <button onClick={()=> dispatch(reset())}>reset</button>  
     </>
   )
 }
