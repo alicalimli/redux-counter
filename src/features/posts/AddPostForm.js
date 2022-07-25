69c1303072a059813ae83553a889dd466a374241
alicalimli <alicalimli76@gmail.com>
2022-07-25 10:54:45 +0300

    added authors option

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,17 +1,25 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
-
+import { useDispatch, useSelector } from "react-redux";
+import { selectUsers } from "../users/usersSlice";
 import { addPost } from "./postsSlice";
 
 const AddPostForm = () => {
+  const users = useSelector(selectUsers);
   const dispatch = useDispatch();
 
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
 
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
+  const onAuthorChanged = (e) => setContent(e.target.value);
+
+  const authorOptions = users.map((user) => (
+    <option key={user.name + user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
 
   const handleAddPost = (e) => {
     e.preventDefault();
@@ -27,11 +35,17 @@ const AddPostForm = () => {
     <section>
       <h2>Add a New Post</h2>
       <form onSubmit={handleAddPost}>
+        <label htmlFor="author-name">Author</label>
+        <select required>
+          <option value=""></option>
+          {authorOptions}
+        </select>
         <label htmlFor="postTitle">Post Title:</label>
         <input
           type="text"
           id="postTitle"
           name="postTitle"
+          required
           value={title}
           onChange={onTitleChanged}
         />
@@ -39,6 +53,7 @@ const AddPostForm = () => {
         <textarea
           id="postContent"
           name="postContent"
+          required
           value={content}
           onChange={onContentChanged}
         />
