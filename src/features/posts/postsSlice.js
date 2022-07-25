ba4a3b8aa0bbd5de008c89b1b099624eda1e9e27
alicalimli <alicalimli76@gmail.com>
2022-07-25 12:11:34 +0300

    added addReaction action

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -3,19 +3,32 @@ import { sub } from "date-fns";
 
 const initialState = [
   {
-    id: 1,
-    title: "blahblahblah",
-    content: "asfsdf",
+    id: "1",
+    title: "Learning Redux Toolkit",
+    content: "I've heard good things.",
     date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
   {
-    id: 2,
-    title: "blahblahblah",
-    content: "asfsdf",
-    date: sub(new Date(), { minutes: 20 }).toISOString(),
+    id: "2",
+    title: "Slices...",
+    content: "The more I say slice, the more I want pizza.",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
 ];
-
 const postsSlice = createSlice({
   name: "posts",
   initialState,
@@ -37,11 +50,19 @@ const postsSlice = createSlice({
         };
       },
     },
+    addReaction: (state, action) => {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.posts.find((post) => post.id === post.id);
+      if (existingPost) {
+        console.log(existingPost.reaction[reaction]);
+        existingPost.reaction[reaction]++;
+      }
+    },
   },
 });
 
 export const selectPosts = (state) => state.posts;
 
-export const { addPost } = postsSlice.actions;
+export const { addPost, addReaction } = postsSlice.actions;
 
 export default postsSlice.reducer;
